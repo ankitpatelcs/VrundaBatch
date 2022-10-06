d216ed0dfe1b770ca6c6ff4784a22092b729fa2d
Ankit <ankitpatel.tops@gmail.com>
2022-10-06 10:49:56 +0530

    route values

---
+++ FirstApp/FirstApp/Controllers/FirstController.cs
@@ -0,0 +1,36 @@
+﻿using FirstApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirstApp.Controllers
+{
+    public class FirstController : Controller
+    {
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Index()
+        {
+            List<Employee> li = new List<Employee>();
+            li.Add(new Employee { empid=1, fname="Vrunda", email="v@gmail.com" });
+            li.Add(new Employee { empid=2, fname="Siddhi", email="v@gmail.com" });
+            li.Add(new Employee { empid=3, fname="Chirag", email="v@gmail.com" });
+            li.Add(new Employee { empid=4, fname="Parth", email="v@gmail.com" });
+
+            return View(li);
+        }
+
+        public IActionResult RouteValues()
+        {
+            ViewBag.controller = RouteData.Values["controller"].ToString();
+            ViewBag.action = RouteData.Values["action"].ToString();
+            ViewBag.id = RouteData.Values["id"].ToString();
+            return View();
+        }
+    }
+}
---
+++ ReturntypeofAction/ReturntypeofAction/Controllers/DefaultController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReturntypeofAction.Controllers
+{
+    public class DefaultController : Controller
+    {
+        private IWebHostEnvironment _env;
+        public DefaultController(IWebHostEnvironment env)
+        {
+            _env = env;
+        }
+        public ContentResult Index()
+        {
+            //return Content("Learning <b>.Net Core</b> from TOPS","text/html");
+            return Content(_env.WebRootPath);
+        }
+
+        public ViewResult SendMsg(string msg)
+        {
+            ViewBag.msg = msg;
+            return View();
+        }
+
+        public RedirectResult GotoUrl()
+        {
+            return Redirect("https://www.google.com");
+        }
+        public RedirectResult GotoNextUrl()
+        {
+            return RedirectPermanent("https://www.google.com");
+        }
+    }
+}
